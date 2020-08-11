60e3d298c6d5a4669ee6480a1afabd92702301ba
Potentialize <38586343+potentialize@users.noreply.github.com>
2020-08-12 01:04:35 +0200

    BUGFIX: Prevent font awesome icons from flashing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
+// normalize.css
 import 'normalize.css'
-import ReduxProvider from './src/state/reduxProvider'
+
+// BUGFIX: prevent font awesome icons from flashing
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 // add redux provider
+import ReduxProvider from './src/state/reduxProvider'
 export const wrapRootElement = ReduxProvider
